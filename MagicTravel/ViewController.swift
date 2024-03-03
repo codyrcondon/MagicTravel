e2f72390121e478677ef16295b272a2a3e94ef55
Cody Condon <codycondon@codys-air.home>
2024-03-03 17:16:28 -0500

    first commit

---
+++ MagicTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicTravel
+//
+//  Created by Cody Condon on 2024-03-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
